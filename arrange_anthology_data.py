f667ae14d0ca975873a8ce02c98698f953b2429c
Abheesht Sharma <sharmabhee@gmail.com>
2021-03-17 17:20:13 +0530

    Fix logical errors, with year at top of hierarchy

--- arrange_anthology_data.py
+++ arrange_anthology_data.py
@@ -3,7 +3,6 @@ import argparse
 import glob
 from tqdm.auto import tqdm
 import xml.etree.ElementTree as ET
-import argparse
 import json
 
 def innertext(elt):
@@ -17,7 +16,7 @@ def main():
 	json_save_path = args.json_save_path
 	split_path = os.path.split(json_save_path)
 	if not os.path.exists(split_path[0]):
-		os.makedirs(split_path[0]) 
+		os.makedirs(split_path[0])
 
 	cmd = "git clone {}".format("https://github.com/acl-org/acl-anthology.git")
 	print("Running " + cmd)
@@ -36,43 +35,68 @@ def main():
 			tree = ET.fromstring(xml)
 
 			for vol_node in tree.iter('volume'):
-			  
+				
+				# year
+				year = vol_node.find(".//year")
+				if year is None:
+					continue
+				else:
+					year = innertext(year)
+				
+				if year not in all_conf:
+					all_conf[year] = {}
+				
+				# publisher
 				publisher = vol_node.find(".//publisher")
 				if publisher is None:
-					continue
+					publisher_keys = list(all_conf[year].keys())
+					max_anon = -1
+					for publisher_key in publisher_keys:
+						if publisher_key.startswith("anonymous"):
+							max_anon = max(int(publisher_key.split("_")[1]),max_anon)
+					if max_anon == -1:
+						publisher = "anonymous_1"
+					else:
+						publisher = "anonymous_" + str(max_anon+1)
 				else:
 					publisher = innertext(publisher)
-				
-				if publisher not in all_conf:
-					all_conf[publisher] = {}
-				
-				try:
-					year = innertext(vol_node.find(".//year"))
-				except:
-					continue
-				if year not in all_conf[publisher]:
-					all_conf[publisher][year] = {}
-				
+
+				if publisher not in all_conf[year]:
+					all_conf[year][publisher] = {}
+
+				# booktitle
 				booktitle = vol_node.find(".//booktitle")
-				if booktitle is not None:
-					all_conf[publisher][year]["booktitle"] = innertext(booktitle)
+				if booktitle is None:
+					booktitle_keys = list(all_conf[year][publisher].keys())
+					max_anon = -1
+					for booktitle_key in booktitle_keys:
+						if booktitle_key.startswith("anonymous"):
+							max_anon = max(int(booktitle_key.split("_")[1]),max_anon)
+					if max_anon == -1:
+						booktitle = "anonymous_1"
+					else:
+						booktitle = "anonymous_" + str(max_anon+1)
 				else:
-					all_conf[publisher][year]["booktitle"] = None
+					booktitle = innertext(booktitle)
+
+				if booktitle not in all_conf[year][publisher]:
+					all_conf[year][publisher][booktitle] = {}
+
 				
 				month = vol_node.find(".//month")
 				if month is not None:
-					all_conf[publisher][year]["month"] = innertext(month)
+					all_conf[year][publisher][booktitle]["month"] = innertext(month)
 				else:
-					all_conf[publisher][year]["month"] = None
+					all_conf[year][publisher][booktitle]["month"] = None
 
 				url = vol_node.find(".//url")
 				if url is not None:
-					all_conf[publisher][year]["url"] = innertext(url)
+					all_conf[year][publisher][booktitle]["url"] = innertext(url)
 				else:
-					all_conf[publisher][year]["url"] = None
+					all_conf[year][publisher][booktitle]["url"] = None
 				
 				
-				all_conf[publisher][year]["papers"] = []
+				all_conf[year][publisher][booktitle]["papers"] = []
 
 				for node in vol_node.iter('paper'):
 					paper_dict = {}
@@ -92,7 +116,7 @@ def main():
 							else:
 								paper_dict[elem.tag] = innertext(elem)
 					
-					all_conf[publisher][year]["papers"].append(paper_dict)
+					all_conf[year][publisher][booktitle]["papers"].append(paper_dict)
 
 	with open(json_save_path, "w") as f:
 		json.dump(all_conf, f)
