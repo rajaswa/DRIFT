d1a1cfefc757a5ee65839164f08d6276b2d19b89
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-05-31 00:02:53 +0530

    Make viz generic

--- src/utils/viz.py
+++ src/utils/viz.py
@@ -11,55 +11,57 @@ nltk.download("wordnet")
 
 
 def pyplot_scatter_embeddings(
-    years, words, embeddings, year_to_color_map=None, save_fig=False
+    x, y, color_by_values, text_annot=None, color_map=None, save_path=None, color_bar_label="Years",
 ):
-    if year_to_color_map is None:
-        sorted_unique_years = np.array(sorted(np.unique(list(map(int, years)))))
+    if color_map is None:
+        sorted_unique_color_values = np.array(sorted(np.unique(list(map(int, color_by_values)))))
 
-        sorted_unique_colors = (sorted_unique_years - np.min(sorted_unique_years)) / (
-            np.max(sorted_unique_years) - np.min(sorted_unique_years)
+        sorted_unique_color_values_rescaled = (sorted_unique_color_values - np.min(sorted_unique_color_values)) / (
+            np.max(sorted_unique_color_values) - np.min(sorted_unique_color_values)
         )
 
-        year_to_color_map = dict(zip(sorted_unique_years, sorted_unique_colors))
+        value_to_color_map = dict(zip(sorted_unique_color_values, sorted_unique_color_values_rescaled))
 
     fig = plt.figure(figsize=(10, 10))
 
     sc = plt.scatter(
-        embeddings[:, 0],
-        embeddings[:, 1],
-        c=list(map(year_to_color_map.get, list(map(int, years)))),
+        x,
+        y,
+        c=list(map(value_to_color_map.get, list(map(int, color_by_values)))),
         alpha=0.5,
         cmap="Spectral",
     )
-    ax = plt.gca()
-    for i, txt in enumerate(words):
-        ax.annotate(txt, (embeddings[i, 0], embeddings[i, 1]))
-    cbar = plt.colorbar(sc, ticks=sorted_unique_colors + 1 / 12, label="Years")
-    cbar.ax.set_yticklabels(sorted_unique_years)
+    
+    if text_annot is not None:
+        ax = plt.gca()
+        for i, txt in enumerate(text_annot):
+            ax.annotate(txt, (x[i], y[i]))
+    cbar = plt.colorbar(sc, ticks=sorted_unique_color_values_rescaled + 1 / 12, label=color_bar_label)
+    cbar.ax.set_yticklabels(sorted_unique_color_values)
     cbar.ax.axes.tick_params(length=0)
-    if save_fig:
-        plt.savefig("Pyplot Scatter Plot.png", bbox_inches="tight")
+    if save_path:
+        plt.savefig(save_path, bbox_inches="tight")
         plt.close()
     return fig
 
 
-def plotly_scatter_embeddings(years, words, embeddings, save_fig=False):
+def plotly_scatter(x, y, color_by_values, text_annot = None,  save_path=None):
 
     fig = px.scatter(
-        x=embeddings[:, 0],
-        y=embeddings[:, 1],
-        color=years,
-        text=words,
+        x=x,
+        y=y,
+        color=color_by_values,
+        text=text_annot,
         color_continuous_scale="Spectral",
     )
 
     fig.update_traces(textposition="top center")
-    if save_fig:
-        fig.write_html("Plotly Scatter Embeddings Plot.html")
+    if save_path:
+        fig.write_html(save_path)
     return fig
 
 
-def word_cloud(words, max_words=100, stop_words=None, save_fig=False):
+def word_cloud(words, max_words=100, stop_words=None, save_path=None):
     """ Returns PIL WordCloud Image """
     if stop_words is None:
         stop_words = set(stopwords.words("english"))
@@ -72,18 +74,18 @@ def word_cloud(words, max_words=100, stop_words=None, save_fig=False):
         random_state=42,
     ).generate(str(words))
 
-    if save_fig:
+    if save_path:
         wordcloud_svg = wordcloud.to_svg(embed_font=True)
-        with open("WordCloud", "w+") as wordcloud_f:
+        with open(save_path, "w+") as wordcloud_f:
             wordcloud_f.write(wordcloud_svg)
     return word_cloud.to_image()
 
 
-def plotly_line_dataframe(df, x_col, y_col, word_col, save_fig=False):
+def plotly_line_dataframe(df, x_col, y_col, word_col, save_path=None):
 
     fig = px.line(
         df, x=x_col, y=y_col, color=word_col, color_continuous_scale="Spectral"
     )
-    if save_fig:
-        fig.write_html("Plotly  Line Plot.html")
+    if save_path:
+        fig.write_html(save_path)
     return fig
