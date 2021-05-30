75d184c0f13038f898a526b7f37c8b39c9cd5c65
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-05-30 16:47:17 +0530

    Add scripts for TWEC Training (#9)
    * Add scripts for TWEC Training
    
    * Add basic scripts to train twec and visualize
    
    * Update Viz Script
    
    * Update settings.json
    
    * Update settings.json
    
    * Add intermediate changes
    
    * Fix requirements.txt
    
    * Fix style
    
    * Re-organize files/modules
    
    * Fix style

---
+++ train_twec.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+from twec.twec import TWEC
+
+
+aligner = TWEC(size=300, siter=10, diter=10, workers=4)
+
+start = time.time()
+# train the compass: the text should be the concatenation of the text from the slices
+aligner.train_compass(
+    "./data/compass.txt", overwrite=True
+)  # keep an eye on the overwrite behaviour
+end = time.time()
+print("Time Taken for TWEC Pre-Training:", (end - start), " ms")
+
+slices = {}
+for file in sorted(os.listdir("./data/")):
+    if file != "compass.txt":
+        start = time.time()
+        slices[file.split(".")[0]] = aligner.train_slice(f"./data/{file}")
+        end = time.time()
+        print("Time Taken for TWEC Fine-tuning:", (end - start), " ms")
---
+++ visualize_vectors.py
@@ -0,0 +1,46 @@
+import pickle as pkl
+
+import matplotlib.pyplot as plt
+import numpy as np
+import umap.umap_ as umap
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+from viz import plotly_scatter_embeddings
+
+
+cache_dir = ".cache"
+
+with open(f"{cache_dir}/vectors", "rb") as f:
+    vectors = pkl.load(f)
+with open(f"{cache_dir}/all_words", "rb") as f:
+    all_words = pkl.load(f)
+with open(f"{cache_dir}/years", "rb") as f:
+    years = pkl.load(f)
+with open(f"{cache_dir}/compass_vectors", "rb") as f:
+    compass_vectors = pkl.load(f)
+
+type = "tsne"
+if type == "pca":
+    pca = PCA(2, random_state=42)
+    compass_embeddings = pca.fit_transform(compass_vectors)
+    embeddings = pca.transform(vectors)
+elif type == "tsne":
+    tsne = TSNE(n_components=2, init="pca", random_state=42)
+    # compass_embeddings = tsne.fit_transform(compass_vectors)
+    embeddings = tsne.fit_transform(vectors)
+elif type == "umap":
+    reducer = umap.UMAP()
+    embeddings = reducer.fit_transform(vectors)
+
+sorted_unique_years = np.array(sorted(np.unique(list(map(int, years)))))
+
+sorted_unique_cs = (sorted_unique_years - np.min(sorted_unique_years)) / (
+    np.max(sorted_unique_years) - np.min(sorted_unique_years)
+)
+
+year_to_color = dict(zip(sorted_unique_years, sorted_unique_cs))
+
+fig = plotly_scatter_embeddings(years, all_words, embeddings, year_to_color)
+
+fig.show()
