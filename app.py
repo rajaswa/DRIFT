cff0bef6ee2d1213750de1ba9ffc01b1e2f18ead
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-06-12 22:20:43 +0530

    Separate methods into a different module

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import glob
-import inspect
 import os
 
 
@@ -32,6 +31,7 @@ from src.utils.statistics import find_productivity, freq_top_k, yake_keyword_ext
 from src.utils.viz import embs_for_plotting, plotly_heatmap, plotly_histogram, plotly_scatter
 from train_twec import train
 
+from app_utils import get_default_args, get_productivity_for_range, get_word_pair_sim_bw_models, get_years_from_data_path, read_text_file
 
 # Folder selection not directly support in Streamlit as of now
 # https://github.com/streamlit/streamlit/issues/1019
@@ -49,25 +49,6 @@ np.random.seed(42)
 @st.cache(allow_output_mutation=True)
 def get_df():
     return {}
-
-
-def get_default_args(func):
-    signature = inspect.signature(func)
-    return {
-        k: v.default
-        for k, v in signature.parameters.items()
-        if v.default is not inspect.Parameter.empty
-    }
-
-
-def get_values_from_indices(lst, idx_list):
-    return [lst[idx] for idx in idx_list], [
-        lst[idx] for idx in range(len(lst)) if idx not in idx_list
-    ]
-
-
-def get_component(component_var, typ, params):
-    return component_var.__getattribute__(typ)(**params)
 
 
 def plot(obj, col1, col2, typ="plotly"):
@@ -124,6 +105,10 @@ def display_caching_option():
         caching.clear_cache()
 
 
+def get_component(component_var, typ, params):
+    return component_var.__getattribute__(typ)(**params)
+
+
 def generate_components_from_dict(comp_dict, variable_params):
     vars = {}
 
@@ -142,7 +127,6 @@ def generate_components_from_dict(comp_dict, variable_params):
 
     return vars
 
-
 def generate_analysis_components(analysis_type, variable_params):
     # print(ANALYSIS_METHODS[analysis_type])
     # print(variable_params)
@@ -153,79 +137,6 @@ def generate_analysis_components(analysis_type, variable_params):
         ANALYSIS_METHODS[analysis_type]["COMPONENTS"], variable_params
     )
     return vars
-
-
-def get_years_from_data_path(data_path):
-    years = sorted(
-        [fil.split(".")[0] for fil in os.listdir(data_path) if fil != "compass.txt"]
-    )
-    return years
-
-
-def read_text_file(data_path, name):
-    with open(os.path.join(data_path, name + ".txt"), encoding="utf-8") as f:
-        words = f.read()
-    return words
-
-
-def get_productivity_for_range(
-    start_year, end_year, selected_ngrams, years, data_path, n
-):
-    yearss = []
-    words = []
-    prodss = []
-    start_year_idx = years.index(start_year)
-    end_year_idx = years.index(end_year)
-    for year_idx in range(start_year_idx, end_year_idx + 1):
-        year = years[year_idx]
-        year_text = read_text_file(data_path, year)
-        prods = find_productivity(selected_ngrams, year_text, n)
-        for word, productivity in prods.items():
-            yearss.append(year)
-            words.append(word)
-            prodss.append(productivity)
-    productivity_df = pd.DataFrame.from_dict(
-        {"Year": yearss, "Word": words, "Productivity": prodss}
-    )
-    return productivity_df
-
-
-def get_acceleration_bw_models(
-    year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
-):
-    model_path1 = os.path.join(model_path, year1 + ".model")
-    model_path2 = os.path.join(model_path, year2 + ".model")
-
-    word_pairs, em1, em2 = compute_acc_between_years(
-        selected_ngrams,
-        model_path1,
-        model_path2,
-        all_model_vectors=all_model_vectors,
-        top_k_acc=top_k_acc,
-        skip_same_word_pairs=True,
-        skip_duplicates=True,
-    )
-    return word_pairs, em1, em2
-
-
-def get_word_pair_sim_bw_models(
-    year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
-):
-    word_pairs, em1, em2 = get_acceleration_bw_models(
-        year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
-    )
-    word_pair_sim_df = pd.DataFrame(
-        list(word_pairs.items()), columns=["Word Pair", "Acceleration"]
-    )
-    word_pair_sim_df = word_pair_sim_df.sort_values(by="Acceleration", ascending=False)
-
-    word_pair_sim_df_words = []
-    for word1, word2 in word_pair_sim_df["Word Pair"].values:
-        if word1 not in word_pair_sim_df_words:
-            word_pair_sim_df_words.append(word1)
-        if word2 not in word_pair_sim_df_words:
-            word_pair_sim_df_words.append(word2)
-    return word_pair_sim_df, word_pair_sim_df_words
 
 
 st.set_page_config(
---
+++ app_utils.py
@@ -0,0 +1,87 @@
+import os
+import inspect
+
+def get_default_args(func):
+    signature = inspect.signature(func)
+    return {
+        k: v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
+
+
+def get_values_from_indices(lst, idx_list):
+    return [lst[idx] for idx in idx_list], [
+        lst[idx] for idx in range(len(lst)) if idx not in idx_list
+    ]
+
+
+def get_years_from_data_path(data_path):
+    years = sorted(
+        [fil.split(".")[0] for fil in os.listdir(data_path) if fil != "compass.txt"]
+    )
+    return years
+
+def get_productivity_for_range(
+    start_year, end_year, selected_ngrams, years, data_path, n
+):
+    yearss = []
+    words = []
+    prodss = []
+    start_year_idx = years.index(start_year)
+    end_year_idx = years.index(end_year)
+    for year_idx in range(start_year_idx, end_year_idx + 1):
+        year = years[year_idx]
+        year_text = read_text_file(data_path, year)
+        prods = find_productivity(selected_ngrams, year_text, n)
+        for word, productivity in prods.items():
+            yearss.append(year)
+            words.append(word)
+            prodss.append(productivity)
+    productivity_df = pd.DataFrame.from_dict(
+        {"Year": yearss, "Word": words, "Productivity": prodss}
+    )
+    return productivity_df
+
+
+def get_acceleration_bw_models(
+    year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
+):
+    model_path1 = os.path.join(model_path, year1 + ".model")
+    model_path2 = os.path.join(model_path, year2 + ".model")
+
+    word_pairs, em1, em2 = compute_acc_between_years(
+        selected_ngrams,
+        model_path1,
+        model_path2,
+        all_model_vectors=all_model_vectors,
+        top_k_acc=top_k_acc,
+        skip_same_word_pairs=True,
+        skip_duplicates=True,
+    )
+    return word_pairs, em1, em2
+
+def get_word_pair_sim_bw_models(
+    year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
+):
+    word_pairs, em1, em2 = get_acceleration_bw_models(
+        year1, year2, model_path, selected_ngrams, all_model_vectors, top_k_acc
+    )
+    word_pair_sim_df = pd.DataFrame(
+        list(word_pairs.items()), columns=["Word Pair", "Acceleration"]
+    )
+    word_pair_sim_df = word_pair_sim_df.sort_values(by="Acceleration", ascending=False)
+
+    word_pair_sim_df_words = []
+    for word1, word2 in word_pair_sim_df["Word Pair"].values:
+        if word1 not in word_pair_sim_df_words:
+            word_pair_sim_df_words.append(word1)
+        if word2 not in word_pair_sim_df_words:
+            word_pair_sim_df_words.append(word2)
+    return word_pair_sim_df, word_pair_sim_df_words
+
+
+def read_text_file(data_path, name):
+    with open(os.path.join(data_path, name + ".txt"), encoding="utf-8") as f:
+        words = f.read()
+    return words
